2ebc31e501454d46bf32ca93c1c940424a6893f3
Viktor Sheverdin <viktor.sheverdin@gmail.com>
2026-03-09 15:48:06 -0700

    added lsp for TS and Lua

--- init.lua
+++ init.lua
@@ -1,1 +1,6 @@
-require("viktor")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("viktor.core")
+require("viktor.lazy")
+require("viktor.lsp")
--- lua/viktor/init.lua
+++
@@ -1,3 +0,0 @@
-require("viktor.lazy")
-require("viktor.core")
-vim.lsp.enable({ "lua_ls", "pyright", "ts_ls" })
